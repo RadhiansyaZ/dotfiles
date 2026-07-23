b57ba2191835565fb5a21d5323cfd4091f8872a3
Radhiansya Zain Antriksa Putra <58207190+RadhiansyaZ@users.noreply.github.com>
2026-07-23 10:25:43 +0700

    feat(wezterm): simplify settings so it use tmux

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -9,6 +9,8 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.color_scheme = "Catppuccin Mocha"
+config.font = wezterm.font_with_fallback({ "JetBrains Mono", "Fira Code", "DengXian" })
+config.font_size = 11
 config.default_domain = "WSL:Debian"
 
 wezterm.on("gui-startup", function(cmd)
